8dc052bb229047bb408ae24c0cbe3a2b34614deb
Miguel Caballero <mcabs3@gmail.com>
2024-08-30 00:47:00 -0500

    better light/dark support

--- components/linkBoard/linkBoard.tsx
+++ components/linkBoard/linkBoard.tsx
@@ -24,42 +24,38 @@ export default function LinkBoard() {
   if (loading) return <Loading />;
 
   return (
-    <>
-      <main className="mx-auto max-w-vp-lg flex flex-col justify-center min-h-screen">
-        <header className="px-4 py-8 flex flex-col items-center justify-center">
-          <Image
-            src="/profile.jpg"
-            alt="An ai generated image of Miguel Caballero"
-            width={150}
-            height={150}
-            className="rounded-full my-8"
-          />
-          <div className="text-center">
-            <h1 className="uppercase text-3xl text-primary-500 font-black tracking-widest">
-              {data.name}
-            </h1>
-            <h2 className="font-thin lowercase">{data.description}</h2>
-          </div>
-        </header>
-        <section>
-          <ul className="max-w-[375px] mx-auto px-8">
-            {data.links.map((link) => (
-              <li key={link.name} className="first-of-type:mt-0 mt-4">
-                <Link
-                  href={link.url}
-                  className="bg-primary-400 font-bold scale-95 hover:scale-100 transition-all border-2 flex items-center justify-center gap-2 py-4 rounded-lg bg-gradient-to-br border-t-primary-400/50 border-l-primary-400/50 border-r-primary-600/50 border-b-primary-600/50 border-t-primary-400/50 dark:border-l-primary-400/50 dark:border-r-secondary-400/50 dark:border-b-secondary-400/50 dark:from-primary-400/5 dark:to-secondary-400/5 dark:text-primary-50 dark:hover:border-t-primary-400/80 dark:hover:border-l-primary-400/80 dark:hover:border-r-secondary-400/80 dark:hover:border-b-secondary-400/80 dark:hover:from-primary-400/30 dark:hover:to-secondary-400/30"
-                >
-                  {!!link.icon && (
-                    <link.icon className="w-4 h-4 fill-accent-950 dark:fill-primary-50" />
-                  )}
-                  {link.name}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </section>
-        {/* <Source /> */}
-      </main>
-    </>
+    <main className="max-w-[600px] shadow-lg dark:shadow-none mx-auto flex flex-col justify-center min-h-screen px-8 bg-accent-50 dark:bg-transparent">
+      <header className="py-8 flex flex-col items-center justify-center">
+        <Image
+          src="/profile.jpg"
+          alt="An ai generated image of Miguel Caballero"
+          width={150}
+          height={150}
+          className="rounded-full my-8"
+        />
+        <div className="text-center">
+          <h1 className="uppercase text-3xl text-primary-500 font-black tracking-widest">
+            {data.name}
+          </h1>
+          <h2 className="font-thin lowercase">{data.description}</h2>
+        </div>
+      </header>
+      <section>
+        <ul className="max-w-[375px] mx-auto">
+          {data.links.map((link) => (
+            <li key={link.name} className="first-of-type:mt-0 mt-4">
+              <Link
+                href={link.url}
+                className="rounded flex gap-4 items-center px-8 py-4 transition-all hover:scale-105 text-accent-500 fill-accent-400 border-2 font-normal tracking-widest border-accent-500/80 bg-accent-500/5 hover:bg-primary-500/10 hover:text-primary-500/80 hover:border-primary-500/80 hover:fill-primary-500/80 dark:bg-gradient-to-br dark:hover:from-primary-500/10 dark:hover:to-secondary-500/10 dark:hover:text-white dark:hover:border-r-secondary-500/80 dark:hover:border-b-secondary-500/80 dark:hover:fill-white"
+              >
+                {!!link.icon && <link.icon className="w-4 h-4 fill-inherit" />}
+                {link.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </section>
+      {/* <Source /> */}
+    </main>
   );
 }
